23da1e1c2a231224413e13ec17f380f97f54424b
Maadhu938 <maadhuavati12@gmail.com>
2026-03-06 09:22:09 +0530

    Day7

--- Day7/LinearSearch.java
+++ Day7/LinearSearch.java
@@ -1,28 +1,22 @@
-import java.util.*;
-public class LinearSearch{
-    public static void main(String [] args){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the size of the array:");
-        int n=sc.nextInt();
-        int [] arr=new int[n];
-        System.out.println("Enter the elements of the array:");
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-        }
-        System.out.println("Enter the element to search:");
-        int key=sc.nextInt();
-        int index=-1;
-        for(int i=0;i<n;i++){
-            if(arr[i]==key){
-                index=i;
-                break;
+public class LinearSearch {
+    public static int LinearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;   // return index if found
             }
         }
-        if(index!=-1){
-            System.out.println("Element found at index: " + index);
-        }
-        else{
-            System.out.println("Element not found");
+        return -1;   // return -1 if element not found
+    }
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50, 60, 70};
+        int target = 70;
+        int result = LinearSearch(arr, target);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Not found");
         }
     }
 }
+//Time Complexity: O(n) - In the worst case, we may have to traverse the entire array to find the target element.
+//Space Complexity: O(1) - We are using a constant amount of space to store the index and target element.
